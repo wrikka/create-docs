a80374565c7c8b2f560859af16a0fbc9a8d7d65a
newkub <newkubise@gmail.com>
2026-09-06 19:29:57 +0700

    feat(create-docs): wire /changelog route when github releases enabled

--- src/runtime/router.tsx
+++ src/runtime/router.tsx
@@ -8,6 +8,7 @@ import type { DocsAppConfig } from "./config";
 import { findApiCollection } from "./context";
 import { DocsLayout } from "./layouts/DocsLayout";
 import { ApiEndpointPage } from "./pages/ApiEndpointPage";
+import { ChangelogPage } from "./pages/ChangelogPage";
 import { CollectionPage } from "./pages/CollectionPage";
 import { DocPage } from "./pages/DocPage";
 import { HomePage } from "./pages/HomePage";
@@ -56,10 +57,21 @@ export function createDocsRouter(config: DocsAppConfig) {
 		},
 	});
 
+	const extraRoutes = [];
+	if (config.github?.releases) {
+		const changelogRoute = createRoute({
+			getParentRoute: () => rootRoute,
+			path: "/changelog",
+			component: ChangelogPage,
+		});
+		extraRoutes.push(changelogRoute);
+	}
+
 	const routeTree = rootRoute.addChildren([
 		indexRoute,
 		collectionRoute,
 		detailRoute,
+		...extraRoutes,
 	]);
 
 	return createRouter({
